dc96912d946b45d0018038fdb35b9a8cfd6aa3e2
Hector Puch <hectorpuchortonoves@gmail.com>
2024-11-23 09:52:36 +0100

    Exercises 01-10 C++ Language

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void main() {
+
 #pragma region ejercicio1
 	std::cout << "Hello World";
 #pragma endregion
@@ -99,5 +100,52 @@ void main() {
 	valorNegado = -numeroUsuario;
 
 	std::cout << "Este es el valor negado de su numero: " << valorNegado;
+#pragma endregion
+
+#pragma region ejercicio8
+	int numeroUsuario;
+
+	std::cout << "Introduce un numero de 6 cifras: ";
+	std::cin >> numeroUsuario;
+
+	int centenasMillar = numeroUsuario / 100000;
+	int decenasMillar = (numeroUsuario / 10000) % 10;
+	int unidadesMillar = (numeroUsuario / 1000) % 10;
+	int centenas = (numeroUsuario / 100) % 10;
+	int decenas = (numeroUsuario / 10) % 10;
+	int unidades = numeroUsuario % 10;
+	
+	std::cout << "Este numero equivale a las centenas de millar: " << centenasMillar << std::endl;
+	std::cout << "Este numero equivale a las decenas de millar: " << decenasMillar << std::endl;
+	std::cout << "Este numero equivale a las unidades de millar: " << unidadesMillar << std::endl;
+	std::cout << "Este numero equivale a las centenas: " << centenas << std::endl;
+	std::cout << "Este numero equivale a las decenas: " << decenas << std::endl;
+	std::cout << "Este numero equivale a las unidades: " << unidades << std::endl;
+#pragma endregion
+
+#pragma region ejercicio9
+float ladoCuadrado;
+std::cout << "Introduce el lado de un cuadrado: ";
+std::cin >> ladoCuadrado;
+
+float areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+std::cout << "Este es el area del cuadrado: " << areaCuadrado;
+#pragma endregion
+
+#pragma region ejercicio10
+float radioCirculo;
+
+std::cout << "Introduce el radio de un circulo: ";
+std::cin >> radioCirculo;
+
+float numeroPI = 3.141592653589793;
+float areaCirculo = numeroPI * pow(radioCirculo,2);
+
+std::cout << "Esta es la area de un circulo: " << areaCirculo << std::endl;
+
+float perimetroCirculo = 2 * numeroPI * radioCirculo;
+
+std::cout << "Este es el perimetro de un circulo: " << perimetroCirculo;
 #pragma endregion
 }
