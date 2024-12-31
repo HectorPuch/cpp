86a0f17a2c3936186138f8bfd4c528175aa9c0c0
Hector Puch <hectorpuchortonoves@gmail.com>
2024-12-31 20:27:11 +0100

    Exercise 12 C++

---
+++ reviewExercises02/ejercicio12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+	float numero;
+
+	std::cout << "Introduzca un número:" << std::endl;
+	std::cin >> numero;
+	std::cout << "" << std::endl;
+
+	std::cout << numero << " * 1 = " << numero * 1 << std::endl;
+	std::cout << numero << " * 2 = " << numero * 2 << std::endl;
+	std::cout << numero << " * 3 = " << numero * 3 << std::endl;
+	std::cout << numero << " * 4 = " << numero * 4 << std::endl;
+	std::cout << numero << " * 5 = " << numero * 5 << std::endl;
+	std::cout << numero << " * 6 = " << numero * 6 << std::endl;
+	std::cout << numero << " * 7 = " << numero * 7 << std::endl;
+	std::cout << numero << " * 8 = " << numero * 8 << std::endl;
+	std::cout << numero << " * 9 = " << numero * 9 << std::endl;
+	std::cout << numero << " * 10 = " << numero * 10 << std::endl;
+
+	return 0;
+}
