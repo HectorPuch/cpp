240429d071d8584040c3ff02ed28456be5dfc63f
Hector Puch <hectorpuchortonoves@gmail.com>
2025-01-29 20:19:26 +0100

    Exercise 01 C++

---
+++ reviewExercises05/ejercicio01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+	int matrix[] = {1, 2, 3, 4};
+	const int length = 4;
+
+	for (int i = 0; i < length; i++)
+	{
+		std::cout << matrix[i];
+	}
+
+	return 0;
+
+}
