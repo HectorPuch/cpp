34c9bb366e11b6bdce68db4883654fe0506b4fc2
Hector Puch <hectorpuchortonoves@gmail.com>
2025-01-31 18:15:29 +0100

    Update Exercise 04 C++

--- reviewExercises05/ejercicio04.cpp
+++ reviewExercises05/ejercicio04.cpp
@@ -4,7 +4,7 @@ int main() {
 
 	const int length = 5; // Longitud del loop for
 
-	int array[5] = { 0, 1, 2, 3, 4 }; // Array de 5 valors inicialitzat
+	int array[length] = { 0, 1, 2, 3, 4 }; // Array de 5 valors inicialitzat
 
 	int suma = 0; // Variable encarregada de la suma dels valors del array
 
