a1d31c17307a4d0a7a5a267e5d52b9188dacd2b2
Hector Puch <hectorpuchortonoves@gmail.com>
2025-03-10 10:18:24 +0100

    Exercise 14 C++

---
+++ reviewExercises03/ejercicio14.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <time.h>
+
+int main() {
+
+	srand(time(NULL));
+
+	int num1 = rand() % 9 + 1, num2 = rand() % 10 + 1;
+	int resultado;
+
+	std::cout << "Cual es el resultado de multiplicar " << num1 << " * " << num2 << " ?\n";
+	std::cin >> resultado;
+
+	if (resultado == (num1 * num2))
+	{
+		std::cout << "Correcto!\n";
+	}
+	else
+	{
+		std::cout << "Incorrecto! El resultado correcto es " << num1 * num2 << ".\n";
+	}
+
+	return 0;
+
+}
