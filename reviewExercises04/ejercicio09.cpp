7bdd9454a4913b35b75597551e22cb778f31cda0
Hector Puch <hectorpuchortonoves@gmail.com>
2025-03-12 22:52:28 +0100

    Exercise 09 C++

--- reviewExercises04/ejercicio09.cpp
+++ reviewExercises04/ejercicio09.cpp
@@ -3,36 +3,39 @@
 
 int main() {
 
-        std::string username = "Tr4ianus", password = "Admin12345.";
-        std::string usernameInput, passwordInput;
+	std::string username = "Tr4ianus", password = "Admin12345.";
+	std::string usernameInput, passwordInput;
 
-        bool authentication = true;
+	bool authentication = true;
 
-        while (authentication)
-        {
-                std::cout << "Username: ";
-                std::cin >> usernameInput;
+	while (authentication)
+	{
+		std::cout << "Username: ";
+		std::cin >> usernameInput;
 
-                std::cout << "Password: ";
-                std::cin >> passwordInput;
+		std::cout << "Password: ";
+		std::cin >> passwordInput;
 
-                if (usernameInput == username && passwordInput == password)
-                {
-                        std::cout << "Nombre de usuario y contraseña correctos.\n";
-                        authentication = false;
-                }
-                else if (usernameInput != username)
-                {
-                        std::cout << "Nombre de usuario incorrecto.\n";
-                }
-                else if (passwordInput != password)
-                {
-                        std::cout << "Contraseña incorrecta.\n";
-                }
-                else
-                {
-                        std::cout << "Nombre de usuario y contraseña incorrectos.\n";
-                }
-        }
+		if (usernameInput != username && passwordInput != password)
+		{
+			std::cout << "Nombre de usuario y contraseña incorrectos.\n";
+		}
+		else if (usernameInput != username)
+		{ 
+			std::cout << "Nombre de usuario incorrecto.\n";
+		}
+		else if (passwordInput != password)
+		{
+			std::cout << "Contraseña incorrecta.\n";
+		}
+		else
+		{
+			std::cout << "Correcto.\n";
+			authentication = false;
+		}
 
-        return 0;
+	}
+
+	return 0;
+
+}
