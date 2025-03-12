13c41f33eb5502af5d5f8994dd7da3c58c835839
Hector Puch <hectorpuchortonoves@gmail.com>
2025-03-12 20:36:06 +0100

    Exercise 09 C++

---
+++ reviewExercises04/ejercicio09.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string username = "Tr4ianus", password = "Admin12345.";
+	std::string usernameInput, passwordInput;
+
+	bool authentication = true;
+
+	while (authentication)
+	{
+		std::cout << "Username: ";
+		std::cin >> usernameInput;
+
+		std::cout << "Password: ";
+		std::cin >> passwordInput;
+
+		if (usernameInput == username && passwordInput == password)
+		{
+			std::cout << "Nombre de usuario y contraseña correctos.\n";
+			authentication = false;
+		}
+		else if (usernameInput != username)
+		{
+			std::cout << "Nombre de usuario incorrecto.\n";
+		}
+		else if (passwordInput != password)
+		{
+			std::cout << "Contraseña incorrecta.\n";
+		}
+		else
+		{
+			std::cout << "Nombre de usuario y contraseña incorrectos.\n";
+		}
+	}
+
+	return 0;
+
+}
