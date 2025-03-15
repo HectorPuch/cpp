2f42a52be985cc233e6a88178ab229d557625855
Hector Puch <hectorpuchortonoves@gmail.com>
2025-03-16 00:31:36 +0100

    Exercise 20 C++

---
+++ reviewExercises04/ejercicio20.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+
+	int numeroUsuario, numeroMayor, numeroMenor, contador = 0;
+
+	bool continuar = true;
+
+	do
+	{
+		std::cout << "Introduzca un numero: ";
+		std::cin >> numeroUsuario;
+
+		std::cout << "Desea seguir introduciendo numeros?\n";
+		std::cout << "0 - Salir.\n";
+		std::cout << "1 - Continuar.\n";
+		std::cin >> continuar;
+
+		if (contador == 0)
+		{
+			numeroMayor = numeroUsuario;
+			numeroMenor = numeroUsuario;
+		}
+
+		if (numeroUsuario > numeroMayor)
+		{
+			numeroMayor = numeroUsuario;
+		}
+		else if (numeroUsuario < numeroMenor)
+		{
+			numeroMenor = numeroUsuario;
+		}
+
+		contador++;
+
+	} while (continuar);
+
+	std::cout << "Este es el numero mayor: " << numeroMayor << std::endl;
+	std::cout << "Este es el numero menor: " << numeroMenor << std::endl;
+
+	return 0;
+
+}
