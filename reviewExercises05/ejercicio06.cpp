bbbdd0ed639a2214b86c10d2de64364e1f574cde
Hector Puch <hectorpuchortonoves@gmail.com>
2025-03-16 02:10:07 +0100

    Exercise 06 C++

---
+++ reviewExercises05/ejercicio06.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <array>
+
+#define LENGTH 5
+
+int main() {
+
+	int array[LENGTH];
+
+	// Pedimos un número para cada posición del array y multiplicarlo por su índice
+	for (int i = 0; i < LENGTH; i++)
+	{
+		std::cout << "Introduzca un numero para la posicion " << i << ": ";
+		std::cin >> array[i]; // Guardamos el número ingresado por el usuario
+		array[i] *= i; // Multiplicamos por la posición
+	}
+
+	std::cout << "Resultados:\n";
+
+	for (int i = 0; i < LENGTH; i++)
+	{
+		std::cout << "Posicion " << i << ": " << array[i] << std::endl;
+	}
+
+	return 0;
+
+}
